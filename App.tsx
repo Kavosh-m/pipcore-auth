7e378f95dcb8ac701ea695259c5405634c220cb5
Kavosh Madadi <kavoshmadadiglk@gmail.com>
2025-05-11 21:23:05 +0330

    - setup simple stack navigator

--- App.tsx
+++ App.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import * as React from 'react';
 import {View, Text} from 'react-native';
+import {createStaticNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
   return (
     <View
       style={{
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'white',
+        backgroundColor: 'cyan',
       }}>
-      <Text style={{color: 'black', fontSize: 18}}>App</Text>
+      <Text>Home Screen</Text>
     </View>
   );
+}
+
+function DetailScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'pink',
+      }}>
+      <Text>Detail Screen</Text>
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screens: {
+    Home: HomeScreen,
+    Detail: DetailScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
 }
